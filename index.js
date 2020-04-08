f40bdc505155db52bee2b030ebdacdcb3dc99446
Vasu Goel <alohanvasu@gmail.com>
2020-04-08 15:29:51 +0530

    Changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express               = require('express')
 const cors                  = require('cors')
 const app                   = express()
-const port                  = 8080
+let port                  =  process.env.PORT || '8080'
 //const HOST                  = '0.0.0.0'
 const fs                    = require('fs')
 //const {getDistrictWiseData} = require('./pool')
