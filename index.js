e2528ab9bf06fc76f00f3b22d4e7f8b51eae3441
Vasu Goel <alohanvasu@gmail.com>
2020-04-08 15:41:58 +0530

    Re-enable cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let port                  =  process.env.PORT || '8080'
 const fs                    = require('fs')
 //const {getDistrictWiseData} = require('./pool')
 
-//app.use(cors())
+app.use(cors())
 //app.use(function(req, res, next) {
 //    res.header("Access-Control-Allow-Origin", "*");
 //    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
